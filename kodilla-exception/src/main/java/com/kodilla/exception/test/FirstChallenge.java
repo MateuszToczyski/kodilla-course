a750c4f60480297ff9d4c48fddee968f3cfe0855
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-02-25 18:40:04 +0100

    Add package: 'com.kodilla.exception.test'

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        int exceptionCounter = 0;
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException ex) {
+            System.out.println("Cannot divide by 0!");
+            exceptionCounter++;
+        } finally {
+            System.out.println("Operation completed with number of exceptions: " + exceptionCounter);
+        }
+    }
+}
