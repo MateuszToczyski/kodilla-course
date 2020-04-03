31eaec160dbd62d94000a64b385a4ed0414042af
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-04-03 17:24:04 +0200

    Add method: findConnectingFlight

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightDatabase.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightDatabase.java
@@ -1,5 +1,10 @@
 package com.kodilla.good.patterns.challenges.flights;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,5 +30,32 @@ public class FlightDatabase {
         return flights.stream()
                 .filter(flight -> flight.getDestinationAirport().equals(airport))
                 .collect(Collectors.toList());
+    }
+
+    public List<Flight> findConnectingFlight(Airport departureAirport, Airport destinationAirport) {
+
+        if(departureAirport == null || destinationAirport == null) {
+            throw new InvalidParameterException("None of the arguments can be null");
+        }
+
+        Flight firstFlight = null;
+        Flight secondFlight = null;
+
+        for(Flight flight1 : flights) {
+            if(flight1.getDepartureAirport().equals(departureAirport)) {
+                for(Flight flight2 : flights) {
+                    if(flight2.getDestinationAirport().equals(destinationAirport)) {
+                        firstFlight = flight1;
+                        secondFlight = flight2;
+                    }
+                }
+            }
+        }
+
+        if(firstFlight != null) {
+            return new ArrayList<>(Arrays.asList(firstFlight, secondFlight));
+        } else {
+            return null;
+        }
     }
 }
